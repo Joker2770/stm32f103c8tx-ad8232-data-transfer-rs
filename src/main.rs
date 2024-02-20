5271693646932719cd3e4e6f4bdfb7ea805663f7
Joker2770 <1214220480@qq.com>
2024-02-20 22:06:03 +0800

    fix: some adjustments. :bug:

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@
 #![no_main]
 #![no_std]
 
+use core::ops::BitXor;
+
 // Halt on panic
 use panic_halt as _;
 
-use cortex_m::singleton;
+// use core::fmt::Write; // for pretty formatting of the output
+
+// use rtt_target::{rprintln, rtt_init_print};
+
 use nb::block;
 
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{
     adc,
-    dma::Half,
     pac,
     prelude::*,
     serial::{Config, Serial},
 };
 use unwrap_infallible::UnwrapInfallible;
-// use rtt_target::{rprintln, rtt_init_print};
 
 #[allow(clippy::empty_loop)]
 #[entry]
@@ -25,6 +28,7 @@ fn main() -> ! {
     // rtt_init_print!();
     // Acquire peripherals
     let p = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
     let mut flash = p.FLASH.constrain();
     let rcc = p.RCC.constrain();
 
@@ -47,10 +51,8 @@ fn main() -> ! {
         adcpre: rcc::AdcPre::DIV2,
     }, &mut flash.acr);*/
 
-    let dma_ch1 = p.DMA1.split().1;
-
     // Setup ADC
-    let adc1 = adc::Adc::adc1(p.ADC1, clocks);
+    let mut adc1 = adc::Adc::adc1(p.ADC1, clocks);
 
     // Prepare the alternate function I/O registers
     let mut afio = p.AFIO.constrain();
@@ -68,38 +70,28 @@ fn main() -> ! {
         p.USART1,
         (tx, rx),
         &mut afio.mapr,
-        Config::default().baudrate(115200.bps()),
+        Config::default().baudrate(921600.bps()),
         &clocks,
     );
 
     // Configure pa0 as an analog input
-    let adc_ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
+    let mut adc_ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
 
-    let adc_dma = adc1.with_dma(adc_ch0, dma_ch1);
-    let buf = singleton!(: [u16; 2] = [0; 2]).unwrap();
-
-    let mut circ_buffer = adc_dma.circ_read(buf);
-
-    while circ_buffer.readable_half().unwrap() != Half::First {}
-
-    let _first_half = circ_buffer.peek(|half, _| *half).unwrap();
-
-    while circ_buffer.readable_half().unwrap() != Half::Second {}
-
-    let _second_half = circ_buffer.peek(|half, _| *half).unwrap();
-
-    let (_buf, adc_dma) = circ_buffer.stop();
-    //  rprintln!("{:?}", _buf);
-    let (_adc1, _adc_ch0, _dma_ch1) = adc_dma.split();
+    //let mut delay = hal::timer::Timer::syst(cp.SYST, &clocks).delay();
+    // or
+    let mut delay = cp.SYST.delay(&clocks);
 
     loop {
-        let sent = singleton!(: [u8; 4] = [0; 4]).unwrap();
+        let data: u16 = adc1.read(&mut adc_ch0).unwrap();
+
+        let sent: &mut [u8; 4] = &mut [0; 4];
         sent[0] = 0xEC;
-        sent[1] = _buf[1].to_be_bytes()[0];
-        sent[2] = _buf[1].to_le_bytes()[0];
-        sent[3] = (sent[1]) ^ (sent[2]);
+        sent[1] = data.to_be_bytes()[0];
+        sent[2] = data.to_le_bytes()[0];
+        sent[3] = sent[1].bitxor(sent[2]);
         for i in sent {
             block!(serial.tx.write(*i)).unwrap_infallible();
         }
+        delay.delay(10.millis());
     }
 }
